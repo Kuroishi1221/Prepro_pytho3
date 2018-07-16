f66715a12737157e271668a19ccf508004528dcc
Kuroishi1221 <mzani1221@gmail.com>
2018-07-16 21:05:29 +0700

    ahh

---
+++ regis.py
@@ -0,0 +1,33 @@
+"""register"""
+def main():
+    """MF"""
+    title = input()
+    name = input()
+    name_2 = input()
+    age = input()
+    clas = input()
+    school = input()
+
+    if title == 'Miss':
+            print("********************"
+                  "Registration Confirm"
+                  "********************"
+                  "Ms. Manee Meemor"
+                  "Age: 15"
+                  "Matthayom: 4"
+                  "Jinda Manee School"
+                  "********************"
+                  "Thank You!")
+
+    elif title == 'Mr.':
+            print("********************"
+                  "Registration Confirm"
+                  "********************"
+                  "Ms. Manee Meemor"
+                  "Age: 15"
+                  "Matthayom: 4"
+                  "Jinda Manee School"
+                  "********************"
+                  "Thank You!")
+
+main()
